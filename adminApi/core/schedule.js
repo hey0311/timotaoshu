27ea4a725fa84c9d9fde2f4ff49fae7dfdaf07ab
hey0311 <hey0311@qq.com>
2022-06-30 22:45:22 +0800

    change schedule

--- adminApi/core/schedule.js
+++ adminApi/core/schedule.js
@@ -1,47 +1,47 @@
-const schedule = require("node-schedule");
-const { oauth, tool, log, wss } = require("../tool/require");
-const { reptileService, ipReptileService, logService } = require("../service/");
+const schedule = require('node-schedule')
+const { oauth, tool, log, wss } = require('../tool/require')
+const { reptileService, ipReptileService, logService } = require('../service/')
 
 /*
  * 规则1  每晚零点定时任务
  * */
-let rule = new schedule.RecurrenceRule();
-rule.hour = 0;
+let rule = new schedule.RecurrenceRule()
+rule.hour = 0
 /*
  * 规则2 每天早上7点定时任务
  * */
-let rule2 = new schedule.RecurrenceRule();
-rule2.hour = 7;
+let rule2 = new schedule.RecurrenceRule()
+rule2.hour = 7
 /*
  * 规则3  每两分钟一次定时任务
  * */
-let rule3 = new schedule.RecurrenceRule();
-rule3.minute = [];
+let rule3 = new schedule.RecurrenceRule()
+rule3.minute = []
 let i3 = 0,
-  length3 = 30;
+  length3 = 30
 for (i3; i3 < length3; i3++) {
-  rule3.minute.push(i3 * 2);
+  rule3.minute.push(i3 * 2)
 }
 /*
  * 规则4  每天凌晨1点和中午13点定时任务
  * */
-let rule4 = new schedule.RecurrenceRule();
-rule4.hour = [1, 13];
-rule4.minute = 0; //必填
+let rule4 = new schedule.RecurrenceRule()
+rule4.hour = [1, 13]
+rule4.minute = 0 //必填
 /*
  * 规则5 凌晨3点的定时任务
  * */
-let rule5 = new schedule.RecurrenceRule();
-rule5.hour = 3;
-rule5.minute = 0; //必填
+let rule5 = new schedule.RecurrenceRule()
+rule5.hour = 3
+rule5.minute = 0 //必填
 /*
  * 规则6 每两个小时一次的定时任务
  * */
-let rule6 = new schedule.RecurrenceRule();
+let rule6 = new schedule.RecurrenceRule()
 // rule6.hour = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 // rule6.hour = [];
-rule6.hour = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22];
-rule6.minute = 0; //必填
+rule6.hour = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
+rule6.minute = 0 //必填
 // let i6 = 0, length6 = 12;
 // for(i6; i6<length6; i6++){
 //     rule6.hour.push(i6*2);
@@ -51,32 +51,32 @@ rule6.minute = 0; //必填
  * 规则4  每天凌晨1点和中午13点定时任务
  * 爬取代理ip，然后去重，然后再检查
  * */
-let j4 = schedule.scheduleJob(rule4, function () {
-  log.debug("定时任务4：" + new Date().Format());
-  wss.broadcast("定时任务4：" + new Date().Format());
-  ipReptileService.startReptile().then(() => {
-    ipReptileService.removeRepeat().then(() => {
-      ipReptileService.check().then(() => {
-        log.debug("ip更新成功，ip更新时间为：" + new Date().Format());
-      });
-    });
-  });
-});
+// let j4 = schedule.scheduleJob(rule4, function () {
+//   log.debug("定时任务4：" + new Date().Format());
+//   wss.broadcast("定时任务4：" + new Date().Format());
+//   ipReptileService.startReptile().then(() => {
+//     ipReptileService.removeRepeat().then(() => {
+//       ipReptileService.check().then(() => {
+//         log.debug("ip更新成功，ip更新时间为：" + new Date().Format());
+//       });
+//     });
+//   });
+// });
 
 /*
  * 规则4  每天凌晨3点
  * 截取所有的服务器日志
  * */
 let j5 = schedule.scheduleJob(rule5, async function () {
-  await logService.splice(1);
+  await logService.splice(1)
   // await logService.splice(2);
   // await logService.splice(3);
-  await logService.splice(4);
+  await logService.splice(4)
   // await logService.splice(5);
   // await logService.splice(6);
   // await logService.splice(7);
   // await logService.splice(8);
-});
+})
 /*
  * 规则6 每两个小时一次的定时任务
  * 1、未爬取的开始爬书
@@ -87,19 +87,17 @@ let j5 = schedule.scheduleJob(rule5, async function () {
  *
  * */
 let j6 = schedule.scheduleJob(rule6, function () {
-  log.debug("定时任务6：" + new Date().Format());
-  wss.broadcast("定时任务6：" + new Date().Format());
+  log.debug('定时任务6：' + new Date().Format())
+  wss.broadcast('定时任务6：' + new Date().Format())
   reptileService
-    .startReptile()
+    .startReptileKeywords()
     .then(() => {
-      log.debug(
-        "定时任务6：章节开始爬取，爬取完毕时间为：" + new Date().Format()
-      );
-      errorCatalogAgain();
+      log.debug('定时任务6：开始爬取，爬取完毕时间为：' + new Date().Format())
+      errorCatalogAgain()
     })
     .catch(() => {
-      errorCatalogAgain();
-    });
+      errorCatalogAgain()
+    })
 
   /*
    * 再一次爬取错误章节  也就是爬取两次
@@ -113,30 +111,30 @@ let j6 = schedule.scheduleJob(rule6, function () {
       //     clearTimeout(set);
       //     set = null;
       // }, 1000 * 300); //5分钟  因为错误章节5分钟一次
-      await tool.sleep(1000 * 300);
+      await tool.sleep(1000 * 300)
       ipReptileService.check().then(() => {
-        errorCatalogReptile();
-      });
-    });
+        errorCatalogReptile()
+      })
+    })
   }
 
   /*
    * 爬取错误章节
    * */
   function errorCatalogReptile(callback) {
-    reptileService.oneKeyRestartCatalog().then(() => {
+    reptileService.startErrorTasks().then(() => {
       log.debug(
-        "定时任务：错误章节开始爬取，爬取完毕时间为：" + new Date().Format()
-      );
-      callback && callback();
-    });
+        '定时任务：错误记录开始爬取，爬取完毕时间为：' + new Date().Format()
+      )
+      callback && callback()
+    })
   }
-});
+})
 
 module.exports = {
   // j,
   // j2
-};
+}
 /*
  * 指定时间执行方法
  *
