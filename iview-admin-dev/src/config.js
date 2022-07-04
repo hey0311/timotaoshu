471ae6018f83ea9ad74de69345b4827e3a474464
hey0311 <hey0311@qq.com>
2022-07-04 20:27:30 +0800

    use env for config

--- iview-admin-dev/src/config.js
+++ iview-admin-dev/src/config.js
@@ -1,4 +1,14 @@
+import config from '../build/config.js'
+console.log('env', config)
 export default {
-  apiServer: 'http://43.134.102.124:3000',
-  wssServer: 'ws://43.134.102.124:8000',
+  //   apiServer: 'http://43.134.102.124:3000',
+  //   wssServer: 'ws://43.134.102.124:8000',
+  apiServer:
+    config.env === 'development'
+      ? 'http://localhost:3000'
+      : 'http://43.134.102.124:3000',
+  wssServer:
+    config.env === 'development'
+      ? 'ws://localhost:8000'
+      : 'ws://43.134.102.124:8000',
 }
