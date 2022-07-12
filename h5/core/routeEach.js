e45e6c0d495b9ff404a82789045665e5fbda835a
hey0311 <hey0311@qq.com>
2022-07-13 00:46:06 +0800

    add h5

--- h5/core/routeEach.js
+++ h5/core/routeEach.js
@@ -1,60 +1,66 @@
-const {fs, path,log} = require('../tool/require');
+const { fs, path, log } = require('../tool/require')
 
-const basePathG = path.join(__dirname, '../routes');
-let arrG = fs.readdirSync(basePathG);
-let basePathStrG = "";
-
+const basePathG = path.join(__dirname, '../routes')
+let arrG = fs.readdirSync(basePathG)
+let basePathStrG = ''
 
 function routeEach(app, pathArr, basePathStr, basePath) {
-    pathArr = pathArr ? pathArr : arrG;
-    basePathStr = basePathStr ? basePathStr : basePathStrG;
-    basePath = basePath ? basePath : basePathG;
+  pathArr = pathArr ? pathArr : arrG
+  console.log(
+    '🚀 ~ file: routeEach.js ~ line 10 ~ routeEach ~ pathArr',
+    pathArr
+  )
+  basePathStr = basePathStr ? basePathStr : basePathStrG
+  basePath = basePath ? basePath : basePathG
 
-    let i, length = pathArr.length;
-    for(i = 0; i<length; i++){
-        let pathStr = path.join(basePath,`${basePathStr}/${pathArr[i]}`);
-        if(!isExists(pathStr)) {   //检查是否有该文件或者目录  没有就继续下一个循环
-            continue;
-        }
-        if(isDir(pathStr)) {        //检查是不是文件夹
-            let arr = fs.readdirSync(pathStr);
-            routeEach(app, arr, `${basePathStr}/${pathArr[i]}`, basePath);
-        }else {
-            let str = "";
-            if(pathArr[i] == 'index.js') {
-                str = `${basePathStr}`;
-            } else {
-                str = `${basePathStr}/${pathArr[i].substring(0, pathArr[i].length - 3)}`;
-            }
-            // if(basePathStr == "/images") {
-            //     app.use(str, require(pathStr));
-            // } else {
-            //     app.all(str, require(pathStr));   //切记不要用app.use
-            // }
-            // log.info(str);
-            // log.error(pathStr);
-            if(basePathStr == '/api') {
-                app.all(str, require(pathStr));
-            } else {
-                app.get(str, require(pathStr));
-            }
-
-        }
+  let i,
+    length = pathArr.length
+  for (i = 0; i < length; i++) {
+    let pathStr = path.join(basePath, `${basePathStr}/${pathArr[i]}`)
+    if (!isExists(pathStr)) {
+      //检查是否有该文件或者目录  没有就继续下一个循环
+      continue
     }
+    if (isDir(pathStr)) {
+      //检查是不是文件夹
+      let arr = fs.readdirSync(pathStr)
+      routeEach(app, arr, `${basePathStr}/${pathArr[i]}`, basePath)
+    } else {
+      let str = ''
+      if (pathArr[i] == 'index.js') {
+        str = `${basePathStr}`
+      } else {
+        str = `${basePathStr}/${pathArr[i].substring(0, pathArr[i].length - 3)}`
+      }
+      // if(basePathStr == "/images") {
+      //     app.use(str, require(pathStr));
+      // } else {
+      //     app.all(str, require(pathStr));   //切记不要用app.use
+      // }
+      // log.info(str);
+      // log.error(pathStr);
+      if (basePathStr == '/api') {
+        app.all(str, require(pathStr))
+      } else {
+        app.get(str, require(pathStr))
+      }
+    }
+  }
 }
 
-function isExists(path){
-    if(fs.existsSync(path)) {
-        return true;
-    }
-    return;
+function isExists(path) {
+  if (fs.existsSync(path)) {
+    return true
+  }
+  return
 }
 
 function isDir(path) {
-    if(fs.existsSync(path) && fs.statSync(path).isDirectory()) {  //先判断存在不存在  再判断文件类型，判断是不是文件夹
-        return true;
-    }
-    return false;
+  if (fs.existsSync(path) && fs.statSync(path).isDirectory()) {
+    //先判断存在不存在  再判断文件类型，判断是不是文件夹
+    return true
+  }
+  return false
 }
 
-module.exports = routeEach;
+module.exports = routeEach
--- h5/routes/home/index.js
+++ h5/routes/home/index.js
@@ -1,50 +1,52 @@
-var express = require('express');
-var router = express.Router();
-var common = require('../../tool/common');
-const { tool, db, log,path,fs, imgUrlConfig } = require("../../tool/require");
-
+var express = require('express')
+var router = express.Router()
+var common = require('../../tool/common')
+const { tool, db, log, path, fs, imgUrlConfig } = require('../../tool/require')
 /* GET home page. */
-router.use('', async function(req, res, next) {
+router.use('', async function (req, res, next) {
+  // let bookIds = "1336,1337,1439,1338,1342,1344,1345,1346,1349,1350,1353";
+  // let result = await common.listBatch('1', bookIds, '');
+  //
+  // if(typeof result == "object" && result.error) {
+  //     result = {
+  //         book:[]
+  //     };
+  // } else if (typeof result == "object") {
+  //     let i = 0, length = result.book.length;
+  //     for(i; i<length; i++) {
+  //         let book = result.book[i];
+  //         if(book.msg == "没有此书") {
+  //             result.book.splice(i,1);
+  //             i--;
+  //             length--;
+  //         }
+  //     }
+  // } else{
+  //     result = {
+  //         book:[]
+  //     };
+  // }
+  //
+  //
+  // res.render('pages/home/index', { myUrl:'/home/index',bookList: result.book });
 
+  /*
+   * 随机一个id 然后取大于这个id的100条 这100条在代码里随机10条
+   * */
 
-    // let bookIds = "1336,1337,1439,1338,1342,1344,1345,1346,1349,1350,1353";
-    // let result = await common.listBatch('1', bookIds, '');
-    //
-    // if(typeof result == "object" && result.error) {
-    //     result = {
-    //         book:[]
-    //     };
-    // } else if (typeof result == "object") {
-    //     let i = 0, length = result.book.length;
-    //     for(i; i<length; i++) {
-    //         let book = result.book[i];
-    //         if(book.msg == "没有此书") {
-    //             result.book.splice(i,1);
-    //             i--;
-    //             length--;
-    //         }
-    //     }
-    // } else{
-    //     result = {
-    //         book:[]
-    //     };
-    // }
-    //
-    //
-    // res.render('pages/home/index', { myUrl:'/home/index',bookList: result.book });
+  // isJin=1 是启用   type=3是爬完了
+  //     let book = await db.query(`SELECT * FROM book WHERE isJin = 1 and type = 3 and id >=
+  // 	((SELECT MAX(id) FROM book where isJin = 1 and type = 3)-(SELECT MIN(id) FROM book where isJin = 1 and type = 3)) * RAND() + (SELECT MIN(id) FROM book where isJin = 1 and type = 3)  LIMIT 10;
+  // `);
+  const id = req.query.id
 
-    /*
-    * 随机一个id 然后取大于这个id的100条 这100条在代码里随机10条
-    * */
+  let book = await db.query(`SELECT * FROM receivemail where id=${id}`)
 
-    // isJin=1 是启用   type=3是爬完了
-//     let book = await db.query(`SELECT * FROM book WHERE isJin = 1 and type = 3 and id >=
-// 	((SELECT MAX(id) FROM book where isJin = 1 and type = 3)-(SELECT MIN(id) FROM book where isJin = 1 and type = 3)) * RAND() + (SELECT MIN(id) FROM book where isJin = 1 and type = 3)  LIMIT 10;
-// `);
+  res.render('pages/home/index', {
+    myUrl: '/home/index',
+    bookList: book,
+    imgUrlConfig,
+  })
+})
 
-    let book = await db.query(`SELECT * FROM book where isJin = 1 and type = 3 ORDER BY RAND() LIMIT 10`)
-
-    res.render('pages/home/index', { myUrl:'/home/index',bookList: book, imgUrlConfig });
-});
-
-module.exports = router;
+module.exports = router
